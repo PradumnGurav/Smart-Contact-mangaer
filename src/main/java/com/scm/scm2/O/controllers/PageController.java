27766a0d3d414678329118c898f22878422d9c26
PradumnGurav <pradumngurav@gmail.com>
2025-12-23 15:20:37 +0530

    Init the progress from here till now stepuing th tailwind

---
+++ src/main/java/com/scm/scm2/O/controllers/PageController.java
@@ -0,0 +1,19 @@
+package com.scm.scm2.O.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("name","Pradumn");
+        model.addAttribute("College","MIT");
+        model.addAttribute("Phone","5637278293");
+        model.addAttribute("Link","https://leetcode.com/u/Pradumn_Gurav/");
+        System.out.println("Home page handler");
+      return "home";
+    }
+
+}
